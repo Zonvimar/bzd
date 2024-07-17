ed7190f65a0957b3ee8621b7760cd43a41ac7a48
zonvimar <shamanolegan@gmail.com>
2024-07-17 12:44:52 +0300

    init

--- src/app/(bzd)/layout.tsx
+++ src/app/(bzd)/layout.tsx
@@ -1,11 +1,13 @@
 import AppNavbar from '@/components/widgets/AppNavbar'
-import React from 'react'
+import React, {Suspense} from 'react'
 
 const AppLayout = async({children}: { children: React.ReactNode }) => {
 
     return (
         <div className={'bg-content'}>
-            <AppNavbar/>
+            <Suspense>
+                <AppNavbar/>
+            </Suspense>
             <div className={`overflow-y-hidden bg-content1`}>
                 <main className={'bg-content3 overflow-auto flex-1 flex flex-col gap-2'}>
                     {children}
